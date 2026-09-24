b7affb109cc3d89db04b3ccf68f7dff99f80c7f1
GudVerm <gudeliusvermessung@web.de>
2026-09-24 07:43:51 +0200

    Admin: R2-Bildvorschau vorübergehend deaktivieren

--- admin/config.js
+++ admin/config.js
@@ -1,1 +1,4 @@
 window.GUDELIUS_CMS_API = "https://gudelius-cms.gudeliusvermessung.workers.dev";
+
+// Vorübergehend Initial-/Fallbackbilder im Admin anzeigen, bis R2-Medien geprüft sind.
+window.GUDELIUS_CMS_MEDIA_ENABLED = false;
